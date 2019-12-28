3b59c010d6955c4f9f8c6854c3f1349e8591869b
xianrenqh <xianrenqh@163.com>
2019-12-28 14:44:06 +0800

    huicmf v2.0

--- application/common.php
+++ application/common.php
@@ -405,6 +405,39 @@ function new_stripslashes($string) {
     return $string;
 }
 
+// 转换大小
+function changeSize($size,$num = 0)
+{
+    $type = ['B','KB','MB','GB','TB'];
+    $i = 0;
+    for ($i; $size >= 1024; $i++){
+        $size /= 1024;
+    }
+    return round($size,$num).$type[$i];// round四舍五入取值$num位小数
+}
+
+// 显示图片
+function getpic($file,$height = 30)
+{
+    //$file = mb_convert_encoding(urldecode($file),'GB2312','UTF-8');
+    $server = PHP_OS;
+    //防止 有些中文windows或linux乱码
+    if($server == "Linux"){
+        $file = urldecode($file);// 有些会不支持iconv 换mb_convert_encoding函数
+    }elseif($server == "WINNT"){
+        $file = iconv('UTF-8','GB2312',urldecode($file));// 有些会不支持iconv 换mb_convert_encoding函数
+    }else{
+        $file = urldecode($file);
+    }
+    if($fileinfo = @getimagesize($file)){
+        $filecontent = file_get_contents($file);
+        $_b64 = chunk_split(base64_encode($filecontent));
+        $pic = 'data:'.$fileinfo['mime'].';base64,'.$_b64;
+        return "<img src='{$pic}' height='{$height}.px'>";
+    }
+    return '';
+}
+
 function public_gethtml($ftype = '', $val = '', $setting = '')
 {
     $fieldtype = $ftype ? $ftype : (isset($_POST['fieldtype'])&&is_string($_POST['fieldtype']) ? safe_replace($_POST['fieldtype']) : 'textarea');
