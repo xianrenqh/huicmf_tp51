4c39a6587e92e4d329403347f80214d0707ebdf5
huihui <qh@toushivip.com>
2020-07-21 12:42:58 +0800

    Update Module.php

--- application/admin/controller/Module.php
+++ application/admin/controller/Module.php
@@ -14,44 +14,46 @@ use think\facade\Cache;
 use think\facade\Request;
 use think\facade\View;
 
-
 class Module extends Common
 {
+
     private $module;
-    
+
     /*模块列表*/
     public function init()
     {
-        
-        
+
+
         define('INSTALL', true);
-        $dirs = $dirs_arr = [];
-        $dirs = glob(APP_PATH . '*', GLOB_ONLYDIR);
+        $dirs     = $dirs_arr = [];
+        $dirs     = glob(APP_PATH.'*', GLOB_ONLYDIR);
+        $dirs_arr = [];
         foreach ($dirs as $d) {
             $dirs_arr[] = basename($d);
         }
-        $total = count($dirs_arr);
-        $dirs_arr = array_chunk($dirs_arr, 500, true);
-        $page = Request::param('page') ? Request::param('page') : 1;
+        $total     = count($dirs_arr);
+        $dirs_arr  = array_chunk($dirs_arr, 500, true);
+        $page      = Request::param('page') ? Request::param('page') : 1;
         $directory = isset($dirs_arr[($page - 1)]) ? $dirs_arr[($page - 1)] : [];
-        $data = Db::name('module')->whereIn('module', $directory)->order('module asc')->select();
+        $data      = Db::name('module')->whereIn('module', $directory)->order('module asc')->select();
         foreach ($data as $val) {
             $modules[$val['module']] = $val;
         }
         View::assign(['directory' => $directory, 'total' => $total, 'data' => $data, 'modules' => $modules]);
+
         return $this->fetch('module_list');
     }
-    
-    
+
     /**
      * 模块安装
+     *
      * @param string $module 模块名
      */
     public function install()
     {
         define('INSTALL', true);
         $this->module = Request::param('module');
-        $module_api = new ModuleApi();
+        $module_api   = new ModuleApi();
         if (Request::param('dosubmit')) {
             if ($module_api->install($this->module)) {
                 Cache::clear();
@@ -60,24 +62,27 @@ class Module extends Common
                 $this->error($module_api->error_msg, url('init'));
             }
         } else {
-            if (!$module_api->check($this->module))
+            if ( ! $module_api->check($this->module)) {
                 $this->error($module_api->error_msg, url('init'), '', 6);
-            $config = include APP_PATH . $this->module . DIRECTORY_SEPARATOR . 'install' . DIRECTORY_SEPARATOR . 'config.inc.php';
-            if (!is_array($config)) {
+            }
+            $config = include APP_PATH.$this->module.DIRECTORY_SEPARATOR.'install'.DIRECTORY_SEPARATOR.'config.inc.php';
+            if ( ! is_array($config)) {
                 $this->error('配置文件错误！', url('init'), '', 6);
             }
             extract($config);
+
             return $this->fetch('module_config', ['config' => $config]);
         }
     }
-    
+
     /**
      * 模块卸载
      */
     public function uninstall()
     {
-        if (!Request::param('module'))
+        if ( ! Request::param('module')) {
             $this->error('模块名称为空！');
+        }
         $module_api = new ModuleApi();
         if ($module_api->uninstall(Request::param('module'))) {
             Cache::clear();
@@ -86,7 +91,7 @@ class Module extends Common
             $this->error($module_api->error_msg, url('init'));
         }
     }
-    
+
     /**
      * 更新模块缓存
      */
@@ -94,14 +99,15 @@ class Module extends Common
     {
         echo '<script type="text/javascript">parent.location.reload();</script>';
     }
-    
+
     function _dir_path($path)
     {
         $path = str_replace('\\', '/', $path);
-        if (substr($path, -1) != '/')
-            $path = $path . '/';
+        if (substr($path, -1) != '/') {
+            $path = $path.'/';
+        }
+
         return $path;
     }
-    
-    
+
 }
