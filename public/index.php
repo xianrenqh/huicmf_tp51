d62b5cce92647f9058966af6f668331e87425a3d
huihui <qh@toushivip.com>
2020-07-21 12:38:01 +0800

    ds

--- public/index.php
+++ public/index.php
@@ -11,6 +11,7 @@
 // [ 应用入口文件 ]
 namespace think;
 
+define('DS', DIRECTORY_SEPARATOR);
 //主机协议
 define('SERVER_PORT', isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443' ? 'https://' : 'http://');
 //当前访问的主机名
@@ -18,12 +19,12 @@ define('HTTP_HOST', (isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : ''))
 //来源
 define('HTTP_REFERER', isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '');
 
-define('APP_PATH', str_replace("\public","",__DIR__ . '\application\\'));
+define('APP_PATH', str_replace(DS."public", "", __DIR__.DS.'application'.DS));
 
 define('ROOT_PATH', dirname(__FILE__)); //这是程序目录常量
 
 // 加载基础文件
-require __DIR__ . '/../thinkphp/base.php';
+require __DIR__.'/../thinkphp/base.php';
 
 // 支持事先使用静态方法设置Request对象和Config对象
 
