0448f2c4376d740801ea4d2a463d0d38b7da9aa1
xianrenqh <xianrenqh@163.com>
2020-07-30 15:34:50 +0800

    test

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -7,6 +7,7 @@
  */
 
 namespace app\admin\controller;
+
 use think\Controller;
 use think\Db;
 use think\facade\Request;
@@ -15,104 +16,128 @@ use think\facade\Session;
 
 class Common extends Controller
 {
-    
+
     public static $ip;
-    public function __construct() {
+
+    public function initialize()
+    {
         self::$ip = getip();
         self::check_admin();
         self::check_priv();
         self::check_ip();
-        if(get_config('admin_log')) self::manage_log();
+        if (get_config('admin_log')) {
+            self::manage_log();
+        }
     }
-    
+
     /**
      * 判断用户是否已经登陆
      */
-    final private function check_admin() {
-        if(Request::module()=='admin' && Request::controller() =='Index' && Request::action() =='login') {
+    final private function check_admin()
+    {
+        if (Request::module() == 'admin' && Request::controller() == 'Index' && Request::action() == 'login') {
             return true;
         } else {
-            $adminid=intval(Cookie::get('adminid'));
-            if(!Session::get('adminid') || !(Session::get('roleid')) || $adminid != Session::get('adminid'))
-                $this->error('请先登录','Index/login');
+            $adminid = intval(Cookie::get('adminid'));
+            if ( ! Session::get('adminid') || ! (Session::get('roleid')) || $adminid != Session::get('adminid')) {
+                $this->error('请先登录', 'Index/login');
+            }
             self::check_referer();
         }
     }
-    
+
     /**
      * 权限判断
      */
-    final private function check_priv() {
-        if(Request::module() =='admin' && Request::controller() =='Index' && in_array(Request::action(),['login','init','index'])) return true;
-        if(Session::get('roleid')== 1) return true;
-        if(strpos(Request::action(), 'public_') === 0) return true;
-        $r = Db::name('admin_role_priv')->where(array('m'=>Request::module(),'c'=>Request::controller(),'a'=>Request::action(),'roleid'=>Session::get('roleid')))->find();
-        if(!$r) {
+    final private function check_priv()
+    {
+        if (Request::module() == 'admin' && Request::controller() == 'Index' && in_array(Request::action(),
+                ['login', 'init', 'index'])) {
+            return true;
+        }
+        if (Session::get('roleid') == 1) {
+            return true;
+        }
+        if (strpos(Request::action(), 'public_') === 0) {
+            return true;
+        }
+        $r = Db::name('admin_role_priv')->where(array('m'      => Request::module(),
+                                                      'c'      => Request::controller(),
+                                                      'a'      => Request::action(),
+                                                      'roleid' => Session::get('roleid')
+        ))->find();
+        if ( ! $r) {
             error2("你没有操作权限~~~");
         }
-        
+
     }
-    
+
     /**
      * 后台IP禁止判断
      */
-    final private function check_ip(){
+    final private function check_ip()
+    {
         $admin_prohibit_ip = get_config('admin_prohibit_ip');
-        if(!$admin_prohibit_ip) return true;
+        if ( ! $admin_prohibit_ip) {
+            return true;
+        }
         $arr = explode(',', $admin_prohibit_ip);
         var_dump($arr);
-        foreach($arr as $val){
+        foreach ($arr as $val) {
             //是否是IP段
-            if(strpos($val,'*')){
-                if(strpos(self::$ip, str_replace('.*', '', $val)) !== false) {
+            if (strpos($val, '*')) {
+                if (strpos(self::$ip, str_replace('.*', '', $val)) !== false) {
                     error2("你在IP禁止段内,禁止访问！~~~");
                 }
-            }else{
+            } else {
                 //不是IP段,用绝对匹配
-                if(self::$ip == $val) {
+                if (self::$ip == $val) {
                     error2("IP地址绝对匹配,禁止访问！~~~");
                 }
             }
         }
     }
-    
-    
+
     /**
      * 检查REFERER
      */
-    final private function check_referer(){
-        if(strpos(Request::action(), 'public_') === 0) return true;
-        if(HTTP_REFERER){
-            if(strpos(HTTP_REFERER, SERVER_PORT.HTTP_HOST) === false){
+    final private function check_referer()
+    {
+        if (strpos(Request::action(), 'public_') === 0) {
+            return true;
+        }
+        if (HTTP_REFERER) {
+            if (strpos(HTTP_REFERER, SERVER_PORT.HTTP_HOST) === false) {
                 //showmsg('非法来源，拒绝访问！', 'stop');
                 $this->error('非法来源，拒绝访问！');
             }
         }
+
         return true;
     }
-    
+
     /**
      * 记录日志
      */
-    final private function manage_log() {
-        if(Request::action() == '' || Request::action() == 'index' || strpos(Request::action(), '_list') || in_array(Request::action(), array('login', 'public_home'))) {
+    final private function manage_log()
+    {
+        if (Request::action() == '' || Request::action() == 'index' || strpos(Request::action(),
+                '_list') || in_array(Request::action(), array('login', 'public_home'))) {
             return false;
-        }else {
-            $adminid = Session::get('adminid');
+        } else {
+            $adminid   = Session::get('adminid');
             $adminname = Session::get('adminname');
-            $url = 'm='.Request::module().'&c='.Request::controller().'&a='.Request::action();
+            $url       = 'm='.Request::module().'&c='.Request::controller().'&a='.Request::action();
             Db::name('admin_log')->insert([
-                'module'=>Request::module(),
-                'action'=>Request::controller(),
-                'adminname'=>$adminname,
-                'adminid'=>$adminid,
-                'querystring'=>$url,
-                'logtime'=>time(),
-                'ip'=>self::$ip
+                'module'      => Request::module(),
+                'action'      => Request::controller(),
+                'adminname'   => $adminname,
+                'adminid'     => $adminid,
+                'querystring' => $url,
+                'logtime'     => time(),
+                'ip'          => self::$ip
             ]);
         }
     }
-    
-    
-    
+
 }
--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -17,32 +17,35 @@ use think\facade\Session;
 use think\facade\View;
 use think\facade\Cache;
 
-
 class Index extends Common
 {
+
     //后台首页
     public function index()
     {
         View::assign(['rolename' => Session::get('rolename'), 'adminname' => Session::get('adminname')]);
+
         return $this->fetch('index');
-        
+
     }
-    
+
     //后台欢迎页
     public function public_welcome()
     {
         $rs = Db::query('select VERSION() as sqlversion');
-        
-        $getinfo = Db::name('admin')->where('adminid',Session::get('adminid'))->find();
-        
-        $getinfo['last_loginip']= !empty(session('last_loginip'))?session('last_loginip'):'第一次登录';
-        $getinfo['last_logintime']= !empty(session('last_logintime'))?date("Y-m-d H:i:s",session('last_logintime')):'第一次登录';
-        
-        return $this->fetch('welcome',[
-            'getinfo'=>$getinfo,'sqlversion'=>$rs[0]['sqlversion']
+
+        $getinfo = Db::name('admin')->where('adminid', Session::get('adminid'))->find();
+
+        $getinfo['last_loginip']   = ! empty(session('last_loginip')) ? session('last_loginip') : '第一次登录';
+        $getinfo['last_logintime'] = ! empty(session('last_logintime')) ? date("Y-m-d H:i:s",
+            session('last_logintime')) : '第一次登录';
+
+        return $this->fetch('welcome', [
+            'getinfo'    => $getinfo,
+            'sqlversion' => $rs[0]['sqlversion']
         ]);
     }
-    
+
     /**
      * 管理员登录
      */
@@ -52,18 +55,20 @@ class Index extends Common
             if (get_config('login_code') == 1) {
                 //判断是否启用验证码
                 $captcha = new Captcha();
-                if (!$captcha->check(Request::param('code'))) {
-                    $this->error('验证码错误，请重试！','index/login');
+                if ( ! $captcha->check(Request::param('code'))) {
+                    $this->error('验证码错误，请重试！', 'index/login');
                 }
             }
-            if (!is_username(Request::param('username')))
-                $this->error('用户名输入不符合规范','index/login');
-            if (!is_password(Request::param('password')))
-                $this->error('密码输入不符合规范','index/login');
+            if ( ! is_username(Request::param('username'))) {
+                $this->error('用户名输入不符合规范', 'index/login');
+            }
+            if ( ! is_password(Request::param('password'))) {
+                $this->error('密码输入不符合规范', 'index/login');
+            }
             $AdminModel = new Admin();
-            $res = $AdminModel->check_admin(Request::param('username'), Request::param('password'));
+            $res        = $AdminModel->check_admin(Request::param('username'), Request::param('password'));
             if (json_decode($res)->{'status'} == 0) {
-                $this->error(json_decode($res)->tips,'index/login');
+                $this->error(json_decode($res)->tips, 'index/login');
             } else {
                 self::public_clear('index');
                 $this->success(json_decode($res)->tips, 'index/index');
@@ -72,7 +77,7 @@ class Index extends Common
             return $this->fetch('login');
         }
     }
-    
+
     /**
      * 管理员退出
      */
@@ -82,34 +87,33 @@ class Index extends Common
         Cookie::clear();
         $this->success('退出成功！', 'index/login');
     }
-    
-    
+
     //清除所有缓存文件
-    public function public_clear($type='')
+    public function public_clear($type = '')
     {
         //Cache::set('configs',null);
         Cache::clear();
         $path = env('RUNTIME_PATH');
         //如果是目录则继续
-        if (!is_dir($path)) {
-            return json(['status'=>0,'message'=>'runtime目录不存在']);
+        if ( ! is_dir($path)) {
+            return json(['status' => 0, 'message' => 'runtime目录不存在']);
         }
-        $p = scandir($path);
+        $p   = scandir($path);
         $arr = ['cache', 'log', 'temp'];
         foreach ($p as $val) {
-            if (!in_array($val, $arr)) {
+            if ( ! in_array($val, $arr)) {
                 continue;
             }
-            if (!is_dir($path . $val)) {
+            if ( ! is_dir($path.$val)) {
                 continue;
             }
-            $dir = $path . $val . '/';
+            $dir = $path.$val.'/';
             //先删除目录下的文件：
             $dh = opendir($dir);
             while ($file = readdir($dh)) {
                 if ($file != "." && $file != "..") {
-                    $fullpath = $dir . $file;
-                    if (!is_dir($fullpath)) {
+                    $fullpath = $dir.$file;
+                    if ( ! is_dir($fullpath)) {
                         @unlink($fullpath);
                     } else {
                         dir_delete($fullpath);
@@ -117,70 +121,73 @@ class Index extends Common
                 }
             }
             closedir($dh);
-            @rmdir($path . $val . '/');
+            @rmdir($path.$val.'/');
         }
-        if($type!='index')
-            return json(['status'=>1,'message'=>'清除缓存成功','url'=>url('index')]);
+        if ($type != 'index') {
+            return json(['status' => 1, 'message' => '清除缓存成功', 'url' => url('index')]);
+        }
     }
-    
-    
+
     //获取后台菜单，并转换成json输出
     public function public_showmenu()
     {
-        if(Cache::get('index_menu_list'.Session::get('roleid'))){
-            $menu_list=Cache::get('index_menu_list'.Session::get('roleid'));
-            $iscache = 1;
-        }else{
-            $menu_list=Db::name('menu')
-                ->field('`id`,`name`,`m`,`c`,`a`,`data`')
-                ->where(['parentid'=>0,'display'=>1])
-                ->order('listorder ASC')
-                ->select();
-            for($i=0;$i<count($menu_list);$i++){
-                $menu_list[$i]['icon']=$menu_list[$i]['data'];
-                $menu_list[$i]['url']='';
-        
+        if (Cache::get('index_menu_list'.Session::get('roleid'))) {
+            $menu_list = Cache::get('index_menu_list'.Session::get('roleid'));
+            $iscache   = 1;
+        } else {
+            $menu_list = Db::name('menu')->field('`id`,`name`,`m`,`c`,`a`,`data`')->where([
+                    'parentid' => 0,
+                    'display'  => 1
+                ])->order('listorder ASC')->select();
+            for ($i = 0; $i < count($menu_list); $i++) {
+                $menu_list[$i]['icon'] = $menu_list[$i]['data'];
+                $menu_list[$i]['url']  = '';
+
             }
-    
+
             foreach ($menu_list as $key => $value) {
-                $child =Db::name('menu')
-                    ->field('`id`,`name`,`m`,`c`,`a`,`data`')
-                    ->where(['parentid'=>$value['id'],'display'=>1])
-                    ->order('listorder ASC')
-                    ->select();
-                for($i=0;$i<count($child);$i++){
-                    $child[$i]['icon']=$child[$i]['data'];
-                    $child[$i]['url']="/".$child[$i]['m']."/".$child[$i]['c']."/".$child[$i]['a']."?".$child[$i]['data'];
+                $child = Db::name('menu')->field('`id`,`name`,`m`,`c`,`a`,`data`')->where([
+                        'parentid' => $value['id'],
+                        'display'  => 1
+                    ])->order('listorder ASC')->select();
+                for ($i = 0; $i < count($child); $i++) {
+                    $child[$i]['icon'] = $child[$i]['data'];
+                    $child[$i]['url']  = "/".$child[$i]['m']."/".$child[$i]['c']."/".$child[$i]['a']."?".$child[$i]['data'];
                 }
-                foreach ($child as $k=>$v) {
-                    if(Session::get('roleid')!=1){
-                        $data = Db::name('admin_role_priv')
-                            ->field('roleid')
-                            ->where(['roleid'=>Session::get('roleid'),'m'=>$v['m'],'c'=>$v['c'],'a'=>$v['a']])
-                            ->find();
-                        if(!$data) unset($child[$k]);
+                foreach ($child as $k => $v) {
+                    if (Session::get('roleid') != 1) {
+                        $data = Db::name('admin_role_priv')->field('roleid')->where([
+                                'roleid' => Session::get('roleid'),
+                                'm'      => $v['m'],
+                                'c'      => $v['c'],
+                                'a'      => $v['a']
+                            ])->find();
+                        if ( ! $data) {
+                            unset($child[$k]);
+                        }
                     }
                 }
-                if($child){
+                $child = (array_values($child));
+                if ($child) {
                     $menu_list[$key]['children'] = $child;
-                }else{
+                } else {
                     unset($menu_list[$key]);
                 }
             }
-            $iscache=0;
-            Cache::set('index_menu_list'.Session::get('roleid'),$menu_list);
+            $iscache = 0;
+            Cache::set('index_menu_list'.Session::get('roleid'), $menu_list);
         }
-        return json(['status'=>0,'msg'=>'ok','data'=>$menu_list]);
+
+        return json(['status' => 0, 'msg' => 'ok', 'data' => $menu_list]);
     }
-    
-    
+
     /**
      * 选择图标
      */
     public function public_checkicon()
     {
         return $this->fetch('public_checkicon');
-        
+
     }
-    
+
 }
